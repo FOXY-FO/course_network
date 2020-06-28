e0889db7ce506a385b64bf92e2cb76927985b4c7
Vlad <foxyfo1703@gmail.com>
2020-06-28 18:29:07 +0300

    get users from the server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,69 +1,37 @@
 import React from "react"
+import * as axios from 'axios'
 import noImage from '../../assets/img/no-user.jpg'
 
 let Users = ({usersPage, follow, unfollow, setUsers}) => {
-    let {users} = usersPage
+    let {users, pageSize} = usersPage
 
     if (!users.length) {
-        setUsers([
-            {
-                id: 1,
-                image: 'https://sun2-3.userapi.com/6ENtooUWVI-dY0S0MIGT4uQsOQ1tY-iXXz9oRQ/4OEEZsXFznQ.jpg',
-                fullName: 'Vlad Balabkin',
-                status: '1703',
-                likesCount: 51,
-                isFollowed: false,
-                location: {
-                    country: 'the USA',
-                    city: 'San Francisco'
-                },
-            },
-            {
-                id: 2,
-                image: 'https://sun9-14.userapi.com/c855620/v855620944/1e99cc/wBXEuAubXdE.jpg',
-                fullName: 'Marina Alexandrovna',
-                status: 'Shoooo?!?!?',
-                likesCount: 321,
-                isFollowed: true,
-                location: {
-                    country: 'Germany',
-                    city: 'Rothenburg ob der Tauber'
-                },
-            },
-            {
-                id: 3,
-                image: 'https://sun9-69.userapi.com/c631426/v631426381/4942b/B2N-KaI48YU.jpg',
-                fullName: 'Alina Surnakova',
-                status: 'Musya Yurevna',
-                likesCount: 52,
-                isFollowed: true,
-                location: {
-                    country: 'Russia',
-                    city: 'Saint-Petersburg'
-                },
-            }
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users?count=' + pageSize)
+            .then(response => {
+                setUsers(response.data.items)
+            })
     }
 
     return (
         <>
-            {users.map(u => <div>
+            {users.map(u => (
                 <div key={u.id}>
                     <div>
-                        <img src={u.image ? u.image : noImage} alt=""/>
+                        <div>
+                            <img src={u.photos.small ? u.photos.small : noImage} alt=""/>
+                        </div>
+                        {u.followed ?
+                            <button onClick={() => unfollow(u.id)}>unfollow</button> :
+                            <button onClick={() => follow(u.id)}>follow</button>
+                        }
                     </div>
-                    {u.isFollowed ?
-                        <button onClick={() => unfollow(u.id)}>unfollow</button> :
-                        <button onClick={() => follow(u.id)}>follow</button>
-                    }
+                    <div>
+                        <div>name: {u.name}</div>
+                        <div>status: {u.status}</div>
+                        <div>likes: {u.likesCount}</div>
+                    </div>
                 </div>
-                <div>
-                    <div>name: {u.fullName}</div>
-                    <div>status: {u.status}</div>
-                    <div>location: {u.location.country}, {u.location.city}</div>
-                    <div>likes: {u.likesCount}</div>
-                </div>
-            </div>)}
+            ))}
         </>
     )
 }
