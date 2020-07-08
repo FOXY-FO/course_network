451ec1865cb102bd96a28ec249e1a1179ab28d37
Vlad <foxyfo1703@gmail.com>
2020-07-08 03:25:49 +0300

    added the Preloader component

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,12 +3,14 @@ let UNFOLLOW = 'UNFOLLOW'
 let SET_USERS = 'SET_USERS'
 let SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 let SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+let TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
 
 let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 0,
-    currentPage: 1
+    currentPage: 1,
+    isFetching: false
 }
 
 let usersReducer = (state = initialState, action) => {
@@ -50,6 +52,11 @@ let usersReducer = (state = initialState, action) => {
                 ...state,
                 currentPage: action.value
             }
+        case TOGGLE_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching
+            }
         default:
             return state
     }
@@ -60,5 +67,6 @@ export let unfollowAC = userId => ({type: UNFOLLOW, userId})
 export let setUsersAC = users => ({type: SET_USERS, users})
 export let setTotalUsersCount = value => ({type: SET_TOTAL_USERS_COUNT, value})
 export let setCurrentPage = value => ({type: SET_CURRENT_PAGE, value})
+export let toggleIsFetching = isFetching => ({type: TOGGLE_IS_FETCHING, isFetching })
 
 export default usersReducer
