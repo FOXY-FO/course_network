21a200ac6c9a6e526aa280268509c9a441ad1a02
Vlad <foxyfo1703@gmail.com>
2020-07-08 12:08:57 +0300

    (58) refactored mapDispatchToProps

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,79 +1,75 @@
-import {connect} from "react-redux";
+import { connect } from "react-redux"
 import {
-    followAC,
-    setCurrentPage,
-    setTotalUsersCount,
-    setUsersAC,
-    toggleIsFetching,
-    unfollowAC
-} from "../../redux/users-reducer";
-import React from "react";
-import * as axios from "axios";
-import Users from "./Users";
-import Preloader from "../UI/Preloader/Preloader";
+  follow,
+  setCurrentPage,
+  setTotalUsersCount,
+  setUsers,
+  toggleFetching,
+  unfollow,
+} from "../../redux/users-reducer"
+import React from "react"
+import * as axios from "axios"
+import Users from "./Users"
+import Preloader from "../UI/Preloader/Preloader"
 
 class UsersAPIComponent extends React.Component {
-    componentDidMount() {
-        this.props.toggleFetching(true)
-        axios.get('https://social-network.samuraijs.com/api/1.0/users?count=' + this.props.usersPage.pageSize)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-                this.props.toggleFetching(false)
-            })
+  componentDidMount() {
+    this.props.toggleFetching(true)
+    axios
+      .get(
+        "https://social-network.samuraijs.com/api/1.0/users?count=" +
+          this.props.usersPage.pageSize
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items)
+        this.props.setTotalUsersCount(response.data.totalCount)
+        this.props.toggleFetching(false)
+      })
+  }
+
+  onPageChange = (page) => {
+    this.props.toggleFetching(true)
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersPage.pageSize}&page=${page}`
+      )
+      .then((res) => {
+        this.props.setUsers(res.data.items)
+        this.props.setTotalUsersCount(res.data.totalCount)
+        this.props.setCurrentPage(page)
+        this.props.toggleFetching(false)
+      })
+  }
+
+  render() {
+    let { usersPage, follow, unfollow } = this.props
+
+    if (usersPage.isFetching) {
+      return <Preloader />
     }
 
-    onPageChange = page => {
-        this.props.toggleFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersPage.pageSize}&page=${page}`)
-            .then(res => {
-                this.props.setUsers(res.data.items)
-                this.props.setTotalUsersCount(res.data.totalCount)
-                this.props.setCurrentPage(page)
-                this.props.toggleFetching(false)
-            })
-    }
-
-    render() {
-        let {usersPage, follow, unfollow} = this.props
-
-        if (usersPage.isFetching) {
-            return <Preloader/>
-        }
-
-        return <Users
-            usersPage={usersPage}
-            onPageChange={this.onPageChange}
-            unfollow={unfollow} follow={follow}
-        />
-    }
+    return (
+      <Users
+        usersPage={usersPage}
+        onPageChange={this.onPageChange}
+        unfollow={unfollow}
+        follow={follow}
+      />
+    )
+  }
 }
 
-let mapStateToProps = state => ({
-    usersPage: state.usersPage
+let mapStateToProps = (state) => ({
+  usersPage: state.usersPage,
 })
 
-let mapDispatchToProps = dispatch => ({
-    follow: userId => {
-        dispatch(followAC(userId))
-    },
-    unfollow: userId => {
-        dispatch(unfollowAC(userId))
-    },
-    setUsers: users => {
-        dispatch(setUsersAC(users))
-    },
-    setTotalUsersCount: value => {
-        dispatch(setTotalUsersCount(value))
-    },
-    setCurrentPage: value => {
-        dispatch(setCurrentPage(value))
-    },
-    toggleFetching: isFetching => {
-        dispatch(toggleIsFetching(isFetching))
-    }
-})
-
-let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
+let UsersContainer = connect(mapStateToProps, {
+  follow,
+  unfollow,
+  setUsers,
+  setTotalUsersCount,
+  setCurrentPage,
+  toggleFetching,
+})(UsersAPIComponent)
 
 export default UsersContainer
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,72 +1,78 @@
-let FOLLOW = 'FOLLOW'
-let UNFOLLOW = 'UNFOLLOW'
-let SET_USERS = 'SET_USERS'
-let SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
-let SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
-let TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
+let FOLLOW = "FOLLOW"
+let UNFOLLOW = "UNFOLLOW"
+let SET_USERS = "SET_USERS"
+let SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT"
+let SET_CURRENT_PAGE = "SET_CURRENT_PAGE"
+let TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING"
 
 let initialState = {
-    users: [],
-    pageSize: 5,
-    totalUsersCount: 0,
-    currentPage: 1,
-    isFetching: false
+  users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1,
+  isFetching: false,
 }
 
 let usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FOLLOW:
-            return {
-                ...state,
-                users: state.users.map(user => {
-                    if (user.id === action.userId) {
-                        return {...user, followed: true}
-                    }
+  switch (action.type) {
+    case FOLLOW:
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          if (user.id === action.userId) {
+            return { ...user, followed: true }
+          }
 
-                    return user
-                })
-            }
-        case UNFOLLOW:
-            return {
-                ...state,
-                users: state.users.map(user => {
-                    if (user.id === action.userId) {
-                        return {...user, followed: false}
-                    }
+          return user
+        }),
+      }
+    case UNFOLLOW:
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          if (user.id === action.userId) {
+            return { ...user, followed: false }
+          }
 
-                    return user
-                })
-            }
-        case SET_USERS:
-            return {
-                ...state,
-                users: [...action.users]
-            }
-        case SET_TOTAL_USERS_COUNT:
-            return {
-                ...state,
-                totalUsersCount: action.value
-            }
-        case SET_CURRENT_PAGE:
-            return {
-                ...state,
-                currentPage: action.value
-            }
-        case TOGGLE_IS_FETCHING:
-            return {
-                ...state,
-                isFetching: action.isFetching
-            }
-        default:
-            return state
-    }
+          return user
+        }),
+      }
+    case SET_USERS:
+      return {
+        ...state,
+        users: [...action.users],
+      }
+    case SET_TOTAL_USERS_COUNT:
+      return {
+        ...state,
+        totalUsersCount: action.value,
+      }
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.value,
+      }
+    case TOGGLE_IS_FETCHING:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      }
+    default:
+      return state
+  }
 }
 
-export let followAC = userId => ({type: FOLLOW, userId})
-export let unfollowAC = userId => ({type: UNFOLLOW, userId})
-export let setUsersAC = users => ({type: SET_USERS, users})
-export let setTotalUsersCount = value => ({type: SET_TOTAL_USERS_COUNT, value})
-export let setCurrentPage = value => ({type: SET_CURRENT_PAGE, value})
-export let toggleIsFetching = isFetching => ({type: TOGGLE_IS_FETCHING, isFetching })
+export let follow = (userId) => ({ type: FOLLOW, userId })
+export let unfollow = (userId) => ({ type: UNFOLLOW, userId })
+export let setUsers = (users) => ({ type: SET_USERS, users })
+export let setTotalUsersCount = (value) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  value,
+})
+export let setCurrentPage = (value) => ({ type: SET_CURRENT_PAGE, value })
+export let toggleFetching = (isFetching) => ({
+  type: TOGGLE_IS_FETCHING,
+  isFetching,
+})
 
 export default usersReducer
